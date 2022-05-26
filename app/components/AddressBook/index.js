3febc3c977971b58ad13d15ba77d3e136c95aa15
Jonson <haotiancode@163.com>
2022-05-26 15:02:19 +0800

    提交 通讯录 js

--- app/components/AddressBook/index.js
+++ app/components/AddressBook/index.js
@@ -1,2 +1,10 @@
+/*
+ * @Author: Jonson 
+ * @Date: 2022-05-26 15:01:19 
+ * @Last Modified by: Jonson
+ * @Last Modified time: 2022-05-26 15:01:40
+ * 获取通讯录原生代码
+ */
+
 var ReactNative = require('react-native')
 module.exports = ReactNative.NativeModules.Contacts
